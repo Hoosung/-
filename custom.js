a8298d43628964e9818c28e429a7fd5c90e1bbc6
Hoosung KIM <101113265+Hoosung@users.noreply.github.com>
2022-04-07 04:24:00 +0900

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,12 @@
+$(function(){
+   /* slick slider - welcome */
+   $('.slideshow').slick({
+       infinite: true,
+       dots: false,
+       autoplay: true,
+       autoplaySpeed: 2000,
+       fade: true,
+       speed: 1000,
+       pauseOnHover: false
+   });
+})
